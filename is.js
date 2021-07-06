0717cf9fd713eea5c9866e943c3f0027f3284dec
huggergamez <huggerbertram@gmail.com>
2021-07-06 17:03:32 +0200

    fixed and new description

--- is.js
+++ is.js
@@ -7,18 +7,18 @@ module.exports = function(func) {
 }
 
 
-exports.sus = function(susText) {
+module.exports.sus = function(susText) {
     return susText == 'sus' || susText == 'imposter';
 }
-exports.rich = function(money) {
+module.exports.rich = function(money) {
     return money >= 1000000;
 }
-exports.year = function(year) {
+module.exports.year = function(year) {
     return year == new Date().getFullYear();
 }
-exports.month = function(month) {
+module.exports.month = function(month) {
     return month == new Date().getMonth();
 }
-exports.version = function(version) {
+module.exports.version = function(version) {
     return version == process.version;
 }
